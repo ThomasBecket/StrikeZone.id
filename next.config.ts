1745aad0efec3703b4954c011ef2bbc22489f051
Thomas Becket Tegar Surya Christy <tegarsuryachristy.xrpl.mos@gmail.com>
2026-05-31 18:29:28 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,28 +9,38 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  // Allow access to remote image placeholder.
+  
+  // Allow access to absolute URLs of custom product images.
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**', // This allows any path under the hostname
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
+  
   output: 'standalone',
+  
   transpilePackages: ['motion'],
+  
   webpack: (config, {dev}) => {
+    
     // HMR is disabled in AI Studio via DISABLE_HMR env var.
     // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+    
     if (dev && process.env.DISABLE_HMR === 'true') {
       config.watchOptions = {
         ignored: /.*/,
       };
     }
+    
     return config;
+    
   },
 };
 
